659dd2000f6adecd8ee8d7f0ba028d42c72f1d61
pronoyNath <pronoynath890@gmail.com>
2023-12-22 12:22:32 +0600

    targeted audiendce section added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
+import TargetAudience from "../TargetAudience/TargetAudience";
 import Banner from "./Banner/Banner";
 
 const Home = () => {
     return (
         <div className='max-w-6xl mx-auto'>
            <Banner></Banner>
+           <TargetAudience></TargetAudience>
         </div>
     );
 };
